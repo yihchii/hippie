a3f364cd883729e30be2f15b1165946484785936
Yih-Chii Hwang <yihhwang@mail.med.upenn.edu>
2014-12-04 19:36:05 -0500

    modified correctHiCBias file, bug fixed

--- cmd/correctHiCBias.sh
+++ cmd/correctHiCBias.sh
@@ -16,7 +16,7 @@ fi
 
 # calculate p-values and correct the Hi-C bias of GC content, mappability and length
 echo "[`date`] correcting intra-chromosomal interaction by GC content, mappability, and length"
-"${ETSCRIPT}/estimate_intra_read_pairs_sig.pl" "${GENOME_CHRM}/${RE}_fragment_GC_MAPP_LEN.bed" chr*_95_reads_interaction.txt "intra_Fgc_binned.txt" "intra_L_binned.txt"
+"${ETSCRIPT}/estimate_intra_read_pairs_sig.pl" "${GENOME_CHRM}/${RE}_fragment_GC_MAPP_LEN.bed" chr*_${THRE}_reads_interaction.txt "intra_Fgc_binned.txt" "intra_L_binned.txt"
 
 echo "[`date`] correcting inter-chromosomal interaction by GC content, mappability, and length"
 "${ETSCRIPT}/estimate_inter_read_pairs_sig.pl" "${GENOME_CHRM}/${RE}_fragment_GC_MAPP_LEN.bed" "${LINE}_interChrm_${THRE}_reads_interaction.txt" "inter_Fgc_binned.txt" "inter_L_binned.txt" "${LINE}_interChrm_${THRE}_reads_interaction_pvalue.txt"
