fc3b3481ea293315808aeeaab69b2113bd359620
Yih-Chii Hwang <yihhwang@mail.med.upenn.edu>
2015-04-15 19:47:53 -0400

    Plot the histone enrichment

--- cmd/plotHisEnrichment.sh
+++ cmd/plotHisEnrichment.sh
@@ -3,10 +3,20 @@ source $HIPPIE_INI
 source $HIPPIE_CFG
 
 LINE=$1
+OUT_DIR=$2
 
-export IN="histone_enrichment.txt"
 
 echo "[`date`] plot epigenomics enrichment ${LINE}"
-$RPATH/R --no-save --args "${LINE}" < "${ETSCRIPT}/plot_enrichment.R"
+$RPATH/R --no-save --args "${LINE}" "histone_enrichment.txt" $OUT_DIR < "${ETSCRIPT}/plot_enrichment.R"
 
+EXITSTATUS=$?
+
+if [ !  -s "${OUT_DIR}/${LINE}_histone_enrichment.jpg" ]
+then
+ echo "Incorrect Output!"
+ exit 100
+fi
+
+
+exit $EXITSTATUS
 
