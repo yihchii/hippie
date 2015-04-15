144091b36389d52fa92d1d89c8362dca03cf3b43
Yih-Chii Hwang <yihhwang@mail.med.upenn.edu>
2015-04-15 19:37:19 -0400

    get peak restriction fragments

--- cmd/ETScript/get_peak_fragment.R
+++ cmd/ETScript/get_peak_fragment.R
@@ -5,13 +5,13 @@ threshold_txt <- cmd_args[4];
 threshold <- as.numeric(threshold_txt)/100;
 specific_read <- cmd_args[5];
 nonspecific_read <- cmd_args[6];
-fragment_prefix <- strsplit(specific_read, "\\.")[[1]][1]
+bed_path <- cmd_args[7];
+fragment_prefix <- cmd_args[8];
+size_select <- as.numeric(cmd_args[9]);
 
-peak_fragment <- paste(fragment_prefix,"_",threshold_txt,"_simple.bed",sep = "");
+peak_fragment <- paste(bed_path,"/",fragment_prefix,"_",threshold_txt,"_simple.bed",sep = "");
 
-# specific_read <- ${LINE}_${RE}_fragment_S_reads.bed
-#s_data<-read.table(specific_read,comment.char="",sep = "\t",header=FALSE)
-#names(s_data) <- c("chrm","start","end","readID","reads")
+
 s_data<-read.table(pipe(paste("cut -f1-3,5", specific_read,sep =" ")),comment.char="",sep = "\t",header=FALSE)
 names(s_data) <- c("chrm","start","end","reads")
 
@@ -20,19 +20,19 @@ names(ns_data) <- c("chrm","start","end","reads")
 
 s_data$length <- s_data$end - s_data$start
 
-# d1+d2 <=500 region
-s_data$normReads <- s_data$reads / 500
-#s_data2 <- s_data[c(s_data$length>500),]
+# d1+d2 <=size_select region
+s_data$normReads <- s_data$reads / size_select
+#s_data2 <- s_data[c(s_data$length>size_select),]
 
-s_data$normReads[s_data$length<=500] <- s_data$reads[s_data$length<=500]/s_data$length[s_data$length<=500]
+s_data$normReads[s_data$length<=size_select] <- s_data$reads[s_data$length<=size_select]/s_data$length[s_data$length<=size_select]
 
 
 
 
 ns_data$length <- ns_data$end - ns_data$start
 
-ns_data$normReads <- ns_data$reads / (ns_data$length-500)
-ns_data2 <- ns_data[c(ns_data$length>500),]
+ns_data$normReads <- ns_data$reads / (ns_data$length-size_select)
+ns_data2 <- ns_data[c(ns_data$length>size_select),]
 
 #bg <- sort(ns_data2$normReads)
 
