75e2b67854133d4b0c569cc82bc0e32b6cda56ac
Yih-Chii Hwang <yihhwang@mail.med.upenn.edu>
2015-04-15 19:44:52 -0400

    get the distance to closest restriction site

--- cmd/getDistanceToRS.sh
+++ cmd/getDistanceToRS.sh
@@ -2,24 +2,33 @@
 source $HIPPIE_INI
 source $HIPPIE_CFG
 
-#./README_HOTSPOT.sh ./original_data/GSM455133_30E0LAAXX.1.maq.hic.summary.binned.txt PBALL
-LINE=$1
-FILE=s_${LINE}.bed
+BED_DIR=$1
+TASK=$2
+SIZE_SELECT=$3
+RRSIZE=$4
+RE=$5
 
-
-if [ !  -s "${FILE}" ]
+FILE=${2}_rmdup.bed
+if [ !  -s "${BED_DIR}/${FILE}" ]
 then
- echo "Cannot find input file $FILE!"
+ echo "Cannot find input file ${BED_DIR}/$FILE!"
  exit 100
 fi
 
+if [ !  -s "${GENOME_CHRM_PATH}/${RE}_site.bed" ]
+then
+ echo "Cannot find input file ${GENOME_CHRM_PATH}/${RE}_site.bed!"
+ exit 100
+fi
 
-paste ${FILE} s_${LINE}_left_distToRE.txt s_${LINE}_right_distToRE.txt| awk -v l=${LINE} '{if ($11+$12 <= 500)print > "s_"l"_specific.bed"; else print >"s_"l"_nonspecific.bed";}'
+$PYTHON ${ETSCRIPT}/get_closest_RS_distance.py ${RRSIZE} ${GENOME_CHRM_PATH}/${RE}_site.bed ${BED_DIR}/${FILE} ${BED_DIR}/${2}_rmdup_distToRS.bed
+
+awk -v t=${TASK} -v size=$SIZE_SELECT -v beddir=${BED_DIR} '{if ($11+$12 <= size)print > beddir"/"t"_specific.bed"; else print >beddir"/"t"_nonspecific.bed";}' ${BED_DIR}/${2}_rmdup_distToRS.bed
 
 
 EXITSTATUS=$?
 
-if [ !  -s "s_${LINE}_nonspecific.bed" ]
+if [ !  -s "${BED_DIR}/${TASK}_nonspecific.bed" ]
 then
  echo "Incorrect Output!"
  exit 100
