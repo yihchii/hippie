fe1f90c49c3a39495bc07b64dc6bc2cdf872d7a1
Yih-Chii Hwang <yihhwang@mail.med.upenn.edu>
2015-04-15 18:23:58 -0400

    tailored for STAR mapping output

--- cmd/rmdupBed.sh
+++ cmd/rmdupBed.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
 source $HIPPIE_INI
+source $HIPPIE_CFG
 
+export BED_DIR=$1
+export BEDFILE=$2
 
-
-
-if [ !  -s "${1}.bed" ]
+if [ !  -s "${BED_DIR}/${BEDFILE}.bed" ]
 then
- echo "Cannot find input file ${1}.bed!"
+ echo "Cannot find input file ${BED_DIR}/${BEDFILE}.bed!"
  exit 100
 fi
 
-
-
-sort -T $TMPDIR -k1,1 -k2,2n -k3,3n -k4,4 -k5,5n -k6,6n -u ${1}.bed -o ${1}_rmdup.bed
+sort -T $TMPDIR -k1,1 -k2,2n -k5,5 -k6,6n -u ${BED_DIR}/${BEDFILE}.bed -o ${BED_DIR}/${BEDFILE}_rmdup.bed
 
 
 EXITSTATUS=$?
 
-if [ !  -s "${1}_rmdup.bed" ]
+if [ !  -s "${BED_DIR}/${BEDFILE}_rmdup.bed" ]
 then
  echo "Incorrect Output!"
  exit 100
