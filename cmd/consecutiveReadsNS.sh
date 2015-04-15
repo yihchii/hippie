a139e943d7ed7daf42edea42f3b831eee1408205
Yih-Chii Hwang <yihhwang@mail.med.upenn.edu>
2015-04-15 19:44:02 -0400

    concat non-specific reads

--- cmd/consecutiveReadsNS.sh
+++ cmd/consecutiveReadsNS.sh
@@ -2,32 +2,32 @@
 source $HIPPIE_INI
 source $HIPPIE_CFG
 
-FILE=$1
-LINE=$2
+BED_DIR=$1
+FILE=$2
+LINE=$3
 
-
-if [ !  -s "${FILE}" ]
+if [ !  -s "${BED_DIR}/${FILE}" ]
 then
- echo "Cannot find input file $FILE!"
+ echo "Cannot find input file ${BED_DIR}/$FILE!"
  exit 100
 fi
 
 
 echo "[`date`] Concating the file of ${LINE}......"
 
-cut -f1-3,7 $FILE > "${LINE}_concat_NS.txt"
-cut -f4-7 $FILE >> "${LINE}_concat_NS.txt"
+cut -f1-3,9 ${BED_DIR}/$FILE > "${BED_DIR}/${LINE}_concat_NS.txt"
+cut -f5-7,9 ${BED_DIR}/$FILE >> "${BED_DIR}/${LINE}_concat_NS.txt"
 
 echo "[`date`] Generating sorted bed file"
-sort -T $TMPDIR -k 1,1 -k 2,2n "${LINE}_concat_NS.txt" > "${LINE}_concat_NS.bed"
-rm "${LINE}_concat_NS.txt"
+sort -T $TMPDIR -k 1,1 -k 2,2n "${BED_DIR}/${LINE}_concat_NS.txt" > "${BED_DIR}/${LINE}_concat_NS.bed"
+rm "${BED_DIR}/${LINE}_concat_NS.txt"
 
 echo "[`date`] Getting consecutive file of ${LINE}"
-$BEDTOOLS merge -i "${LINE}_concat_NS.bed" -c 4 -o collapse,count > "${LINE}_consecutive_NS.bed"
+$BEDTOOLS merge -i "${BED_DIR}/${LINE}_concat_NS.bed" -c 4 -o collapse,count > "${BED_DIR}/${LINE}_consecutive_NS.bed"
 
 EXITSTATUS=$?
 
-if [ !  -s "${LINE}_consecutive_NS.bed" ]
+if [ !  -s "${BED_DIR}/${LINE}_consecutive_NS.bed" ]
 then
  echo "Incorrect Output!"
  exit 100
