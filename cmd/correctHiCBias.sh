dde00766bdf37359a8b67aba163fda547acd35fc
Yih-Chii Hwang <yihhwang@mail.med.upenn.edu>
2015-04-15 18:39:23 -0400

    Hi-C bias correction by Jin et al. 2014

--- cmd/correctHiCBias.sh
+++ cmd/correctHiCBias.sh
@@ -2,11 +2,21 @@
 source $HIPPIE_INI
 source $HIPPIE_CFG
 
-LINE=$1
-RE=$2
-THRE=$3
+BED_DIR=$1
+LINE=$2
+RE=$3
+THRE=$4
 
-FILE="chr1_${THRE}_reads_interaction.txt"
+cd ${BED_DIR};
+files=()
+for x in `ls`; do
+    if [[ $x =~ ^[A-Za-z0-9]+_${THRE}_reads_interaction.txt ]]; then
+				files+=("${BED_DIR}/$x")
+    fi;
+done
+cd -
+
+FILE="${files[0]}"
 
 if [ !  -s "${FILE}" ]
 then
@@ -14,18 +24,21 @@ then
  exit 100
 fi
 
+
 # calculate p-values and correct the Hi-C bias of GC content, mappability and length
-echo "[`date`] correcting intra-chromosomal interaction by GC content, mappability, and length"
-"${ETSCRIPT}/estimate_intra_read_pairs_sig.pl" "${GENOME_CHRM}/${RE}_fragment_GC_MAPP_LEN.bed" chr*_${THRE}_reads_interaction.txt "intra_Fgc_binned.txt" "intra_L_binned.txt"
+for i in "${!files[@]}"; do
+	echo "[`date`] correcting intra-chromosomal interaction by GC content, mappability, and length"
+	"${ETSCRIPT}/estimate_intra_read_pairs_sig.pl" "${GENOME_CHRM_PATH}/${RE}_fragment_GC_MAPP_LEN.bed" ${files[$i]} "${BED_DIR}/intra_Fgc_binned.txt" "${BED_DIR}/intra_L_binned.txt" "${BED_DIR}"
+done
 
 echo "[`date`] correcting inter-chromosomal interaction by GC content, mappability, and length"
-"${ETSCRIPT}/estimate_inter_read_pairs_sig.pl" "${GENOME_CHRM}/${RE}_fragment_GC_MAPP_LEN.bed" "${LINE}_interChrm_${THRE}_reads_interaction.txt" "inter_Fgc_binned.txt" "inter_L_binned.txt" "${LINE}_interChrm_${THRE}_reads_interaction_pvalue.txt"
+"${ETSCRIPT}/estimate_inter_read_pairs_sig.pl" "${GENOME_CHRM_PATH}/${RE}_fragment_GC_MAPP_LEN.bed" "${BED_DIR}/${LINE}_interChrm_${THRE}_reads_interaction.txt" "${BED_DIR}/inter_Fgc_binned.txt" "${BED_DIR}/inter_L_binned.txt" "${BED_DIR}/${LINE}_interChrm_${THRE}_reads_interaction_pvalue.txt"
 
 echo "[`date`] Complete find interaction p-values"
 
 
 EXITSTATUS=$?
-if [ !  -s "${LINE}_interChrm_${THRE}_reads_interaction_pvalue.txt" ]
+if [ !  -s "${BED_DIR}/${LINE}_interChrm_${THRE}_reads_interaction_pvalue.txt" ]
 then
  echo "Incorrect Output!"
  exit 100
