e49fd2aa63171f35ccc81c7f8339896f6cba6ac5
Yih-Chii Hwang <yihhwang@mail.med.upenn.edu>
2015-04-15 19:46:24 -0400

    get number of reads piled up on the restristriction fragment

--- cmd/getFragmentsRead.sh
+++ cmd/getFragmentsRead.sh
@@ -2,28 +2,49 @@
 source $HIPPIE_INI
 source $HIPPIE_CFG
 
-LINE=$1
-RE=$2
-FILE=s_${LINE}.bed
+BED_DIR=$1
+LINE=$2
+RE=$3
+FILE_S=${LINE}_concat_S.bed
+FILE_NS=${LINE}_concat_NS.bed
 
 
-if [ !  -s "${FILE}" ]
+if [ !  -s "${BED_DIR}/${FILE_S}" ]
 then
- echo "Cannot find input file $FILE!"
+ echo "Cannot find input file ${BED_DIR}/$FILE_S!"
+ exit 100
+fi
+
+if [ !  -s "${BED_DIR}/${FILE_NS}" ]
+then
+ echo "Cannot find input file ${BED_DIR}/$FILE_NS!"
+ exit 100
+fi
+
+if [ !  -s "${GENOME_CHRM_PATH}/${RE}_fragments.bed" ]
+then
+ echo "Cannot find restriction fragment file ${GENOME_CHRM_PATH}/${RE}_fragments.bed!"
  exit 100
 fi
 
 
+
 ## map concated paired-end reads to restriction fragments
 
-$BEDTOOLS intersect -sorted -wa -wb -a ${GENOME_CHRM}/${RE}_fragments.bed -b ${LINE}_concat_m500.bed -loj|awk 'BEGIN{OFS="\t";} {print $1,$2,$3,$7}'|$BEDTOOLS merge -i stdin -c 4 -o collapse,count | awk 'BEGIN{OFS="\t"}{if($4~/^\.$/)print $1,$2,$3,$4,0;else print $0}'> ${RE}_fragment_S_reads.bed
+$BEDTOOLS intersect -sorted -wa -wb -a ${GENOME_CHRM_PATH}/${RE}_fragments.bed -b ${BED_DIR}/${FILE_S} -loj|awk 'BEGIN{OFS="\t";} {print $1,$2,$3,$7}'|$BEDTOOLS merge -i stdin -c 4 -o collapse,count | awk 'BEGIN{OFS="\t"}{if($4~/^\.$/)print $1,$2,$3,$4,0;else print $0}'> ${BED_DIR}/${RE}_fragment_S_reads.bed
 
-$BEDTOOLS intersect -sorted -wa -wb -a ${GENOME_CHRM}/${RE}_fragments.bed -b ${LINE}_concat_NS.bed -loj|awk 'BEGIN{OFS="\t";}{print $1,$2,$3,$7}'|$BEDTOOLS merge -i - -c 4 -o collapse,count | awk 'BEGIN{OFS="\t"}{if($4~/^\.$/)print $1,$2,$3,$4,0;else print $0}'> ${RE}_fragment_NS_reads.bed
+$BEDTOOLS intersect -sorted -wa -wb -a ${GENOME_CHRM_PATH}/${RE}_fragments.bed -b ${BED_DIR}/${FILE_NS} -loj|awk 'BEGIN{OFS="\t";}{print $1,$2,$3,$7}'|$BEDTOOLS merge -i - -c 4 -o collapse,count | awk 'BEGIN{OFS="\t"}{if($4~/^\.$/)print $1,$2,$3,$4,0;else print $0}'> ${BED_DIR}/${RE}_fragment_NS_reads.bed
 
 
 EXITSTATUS=$?
 
-if [ !  -s "${RE}_fragment_NS_reads.bed" ]
+if [ !  -s "${BED_DIR}/${RE}_fragment_NS_reads.bed" ]
+then
+ echo "Incorrect Output!"
+ exit 100
+fi
+
+if [ !  -s "${BED_DIR}/${RE}_fragment_S_reads.bed" ]
 then
  echo "Incorrect Output!"
  exit 100
