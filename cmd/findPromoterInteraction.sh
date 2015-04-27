b0889b0a64843fa72c81585c425174b5458473c3
yihchii <yihhwang@mail.med.upenn.edu>
2015-04-27 11:08:14 -0400

    remove redundant commands

--- cmd/findPromoterInteraction.sh
+++ cmd/findPromoterInteraction.sh
@@ -11,8 +11,8 @@ FILE="${BED_DIR}/${LINE}_${RE}fragment_${THRE}_promoter_annotated.bed"
 
 if [ !  -s "${FILE}" ]
 then
- echo "Cannot find input file $FILE!"
- exit 100
+	echo "Cannot find input file $FILE!"
+	exit 100
 fi
 
 echo "[`date`] split promoter_annotated to chromosomes "
@@ -23,19 +23,16 @@ awk -v thre=${THRE} -v beddir=${BED_DIR} -F'\t' 'BEGIN{OFS="\t"}{print > beddir"
 echo "[`date`] start finding intra-chromosomal interaction"
 
 
-chrm=({1..22} "X" "Y")
-
-for x in ${chrm[@]}; do
-	echo "Find promoter interaction for chr${x}"
-  # 2014/01/28:
-  # edit promoterInteractionP7.pl to keep only promoter involved interations 
-  # skip promoterAnno, directly get promoterInteractions
-	"${ETSCRIPT}/promoterInteractionP7.pl" "${BED_DIR}/chr${x}_${THRE}_promoter_annotated.bed" "${BED_DIR}/chr${x}_${THRE}_promoter_annotated_promoterInteraction.txt"
-	# 2014/09/21: add in p-values for promoter interactions
-	echo $x;
-	awk 'BEGIN{OFS="\t"}NR==FNR{a[$1,$2]=$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8"\t"$9"\t"$10"\t"$11"\t"$12"\t"$13;next}{if(($1,$2) in a) print $1,$2,$3,a[$1,$2]}' "${BED_DIR}/chr${x}_${THRE}_promoter_annotated_promoterInteraction.txt" "${BED_DIR}/chr${x}_${THRE}_reads_interaction_pvalue.txt" >  "${BED_DIR}/chr${x}_${THRE}_promoter_annotated_promoterInteraction_pvalue.txt"
-	
-done;
+cd ${BED_DIR};
+for x in `\ls`; do
+	if [[ $x =~ ^([A-Za-z0-9]+)_${THRE}_promoter_annotated.bed ]]; then
+		export chrm=${BASH_REMATCH[1]}
+		echo "Find promoter interaction for ${chrm}"
+		"${ETSCRIPT}/promoterInteractionP7.pl" "${BED_DIR}/${x}" "${BED_DIR}/${chrm}_${THRE}_promoter_annotated_promoterInteraction.txt"
+		awk 'BEGIN{OFS="\t"}NR==FNR{a[$1,$2]=$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8"\t"$9"\t"$10"\t"$11"\t"$12"\t"$13;next}{if(($1,$2) in a) print $1,$2,$3,a[$1,$2]}' "${BED_DIR}/${chrm}_${THRE}_promoter_annotated_promoterInteraction.txt" "${BED_DIR}/${chrm}_${THRE}_reads_interaction_pvalue.txt" >  "${BED_DIR}/${chrm}_${THRE}_promoter_annotated_promoterInteraction_pvalue.txt"
+	fi;
+done
+cd -
 
 
 echo "[`date`] start finding inter-chromosomal interaction"
@@ -64,8 +61,8 @@ EXITSTATUS=$?
 
 if [ !  -s "${BED_DIR}/${LINE}_${THRE}_interChrm_promoterInteraction.txt" ]
 then
- echo "Incorrect Output!"
- exit 100
+	echo "Incorrect Output!"
+	exit 100
 fi
 
 exit $EXITSTATUS
